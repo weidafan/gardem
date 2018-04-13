4d557b2bdef3f8c29430840838f02776680d0201
WoT_House <philip.pinola.14@cnu.edu>
2018-04-13 05:39:21 +0000

    add schedule and mobile pag

--- plugins/internal/ledsPlugin.js
+++ plugins/internal/ledsPlugin.js
@@ -1,14 +1,12 @@
 var resources = require('./../../resources/model');
-
 var actuator, interval;
 var model = resources.pi.actuators.leds;
 var pluginName = model.name;
 var localParams = {'simulate': false, 'frequency': 2000};
-var previousVal = model.value;
+
 exports.start = function (params) {
   localParams = params;
   //observe(model); //#A
-// checkStatus(model.value);
   if (localParams.simulate) {
     simulate();
   } else {
@@ -25,24 +23,6 @@ exports.stop = function () {
   console.info('%s plugin stopped!', pluginName);
 };
 
-// function observe(what) {
-//   Object.observe(what, function (changes) {
-//     console.info('Change detected by plugin for %s...', pluginName);
-//     switchOnOff(model.value); //#B
-//   });
-// };
-function checkStatus(currentVal) {
-  if(currentVal != previousVal){
-  switchOnOff(model.value);
-  }
-}
-// function switchOnOff(value) {
-//   if (!localParams.simulate) {
-//     led.write(value === true ? 1 : 0, function () { //#C
-//       console.info('Changed value of %s to %s', pluginName, value);
-//     });
-//   }
-// };
 
 function connectHardware() {
   var Gpio = require('onoff').Gpio;
@@ -58,28 +38,6 @@ function connectHardware() {
     setTimeout(connectHardware, localParams.frequency);	
 };
 
-function simulate() {
-  interval = setInterval(function () {
-    
-    var Gpio = require('onoff').Gpio;
-    actuator = new Gpio(model.gpio, 'out'); //#D
-    console.info('Hardware %s actuator started!', pluginName);
-
-
-    // Switch value on a regular basis
-    if (model.value) {
-      model.value = false;
-      actuator.write(0);
-      console.log('LED ==== LOW');
-      
-    } else {
-      model.value = true;
-      actuator.write(1);
-      console.log('LED ===== HIGH');
-    }
-  }, localParams.frequency);
-  console.info('Simulated %s actuator started!', pluginName);
-};
 
 //#A Observe the model for the LEDs
 //#B Listen for model changes, on changes call switchOnOff
--- servers/http.js
+++ servers/http.js
@@ -7,24 +7,26 @@ var express = require('express'),
   converter = require('./../middleware/converter'),
   cors = require('cors'),
   bodyParser = require('body-parser');
-  var passport = require('passport');
-  var Strategy = require('passport-local').Strategy;
-  var localdb = require('./../localdb/index');
-  // Configure the local strategy for use by Passport.
+var passport = require('passport');
+var Strategy = require('passport-local').Strategy;
+var localdb = require('./../localdb/index');
+
+var Gpio = require('onoff').Gpio;
+// Configure the local strategy for use by Passport.
 //
 // The local strategy require a `verify` function which receives the credentials
 // (`username` and `password`) submitted by the user.  The function must verify
 // that the password is correct and then invoke `cb` with a user object, which
 // will be set at `req.user` in route handlers after authentication.
 passport.use(new Strategy(
-  function(username, password, cb) {
-    localdb.users.findByUsername(username, function(err, user) {
+  function (username, password, cb) {
+    localdb.users.findByUsername(username, function (err, user) {
       if (err) { return cb(err); }
       if (!user) { return cb(null, false); }
       if (user.password != password) { return cb(null, false); }
       return cb(null, user);
     });
-}));
+  }));
 // Configure Passport authenticated session persistence.
 //
 // In order to restore authentication state across HTTP requests, Passport needs
@@ -32,23 +34,23 @@ passport.use(new Strategy(
 // typical implementation of this is as simple as supplying the user ID when
 // serializing, and querying the user record by ID from the database when
 // deserializing.
-passport.serializeUser(function(user, cb) {
+passport.serializeUser(function (user, cb) {
   cb(null, user.id);
 });
 
-passport.deserializeUser(function(id, cb) {
+passport.deserializeUser(function (id, cb) {
   localdb.users.findById(id, function (err, user) {
     if (err) { return cb(err); }
     cb(null, user);
   });
 });
-  // var path = require('path')
+// var path = require('path')
 var app = express();
 // Use application-level middleware for common functionality, including
 // logging, parsing, and session handling.
 app.use(require('morgan')('combined'));
 app.use(require('cookie-parser')());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('./views'));
 app.use(express.static('./resources'));
 app.use(bodyParser.json());
@@ -72,7 +74,7 @@ app.use(converter());
 // });
 const MongoClient = require('mongodb').MongoClient;
 app.set('view engine', 'ejs')
- var db
+var db
 const url = 'mongodb+srv://tommyfan:cnurobot@garderdb-ityx5.mongodb.net'
 MongoClient.connect(url, (err, client) => {
   // ... start the server
@@ -85,61 +87,95 @@ MongoClient.connect(url, (err, client) => {
 })
 app.get('/index', (req, res) => {
   require('connect-ensure-login').ensureLoggedIn(),
-  // res.sendFile(__dirname + '/index.html')
-  // Note: __dirname is directory that contains the JavaScript source code. Try logging it and see what you get!
-  // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
-  //  db.collection('temperature').find().toArray((err, result) => {
-  //  if (err) return console.log(err)
-    res.render('index.ejs',{ user: req.user,resources: resources})
-//  })
+    // res.sendFile(__dirname + '/index.html')
+    // Note: __dirname is directory that contains the JavaScript source code. Try logging it and see what you get!
+    // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
+    //  db.collection('temperature').find().toArray((err, result) => {
+    //  if (err) return console.log(err)
+    res.render('index.ejs', { user: req.user, resources: resources})
+  //  })
 })
 
 // Define routes.
 app.get('/',
-function(req, res) {
-  res.render('login');
-})
+  function (req, res) {
+    res.render('login');
+  })
 
 app.get('/login',
-function(req, res) {
-  res.render('login');
-})
+  function (req, res) {
+    res.render('login');
+  })
 
 
 app.get('/logout',
-function(req, res){
-  req.logout();
-  res.redirect('/');
-})
+  function (req, res) {
+    req.logout();
+    res.redirect('/');
+  })
 
 app.get('/profile',
-require('connect-ensure-login').ensureLoggedIn(),
-function(req, res){
-  res.render('profile', { user: req.user });
-})
+  require('connect-ensure-login').ensureLoggedIn(),
+  function (req, res) {
+    res.render('profile', { user: req.user });
+  })
 
 
-app.post('/login', 
-passport.authenticate('local', { failureRedirect: '/login' }),
-function(req, res) {
-  res.redirect('/index');
-})
+app.post('/login',
+  passport.authenticate('local', { failureRedirect: '/login' }),
+  function (req, res) {
+    res.redirect('/index');
+  })
 // app.get('/pi', function(req, res){
 //     res.sendFile('index.html', { root: './resources' });
 // });
-app.post('/update_data',(req, res) => {
-     res.redirect('/index')
+app.post('/update_data', (req, res) => {
+  res.redirect('/index')
 })
 
-app.post('/openValve',(req, res) => {
-    resources.pi.actuators.valve.status= !resources.pi.actuators.valve.status;
-    console.log("VALVE STATUS CHANGE TO "+resources.pi.actuators.valve.status);
-    res.redirect('/index')
+app.post('/openValve', (req, res) => {
+  resources.pi.actuators.valve.status = !resources.pi.actuators.valve.status;
+  console.log("VALVE STATUS CHANGE TO " + resources.pi.actuators.valve.status);
+  res.redirect('/index')
 })
 
-// app.get('/pi/sensors/temperature', function(req, res){
-//   res.sendFile('temperature.html', { root: './resources' });
-// });
+app.post('/setTime', (req, res) => {
+  resources.pi.timer.weekofdays[0] = req.body.Sunday;
+  resources.pi.timer.weekofdays[1] = req.body.Monday;
+  resources.pi.timer.weekofdays[2] = req.body.Tuesday;
+  resources.pi.timer.weekofdays[3] = req.body.Wednesday;
+  resources.pi.timer.weekofdays[4] = req.body.Thursday;
+  resources.pi.timer.weekofdays[5] = req.body.Friday;
+  resources.pi.timer.weekofdays[6] = req.body.Saturday;
+  resources.pi.timer.hour = parseInt(req.body.hour);
+  resources.pi.timer.minute = parseInt(req.body.minute);
+  //schedule time to turn on stepper.
+  var schedule = require('node-schedule');
+  var rule = new schedule.RecurrenceRule();
+  var days = [];
+  actuator = new Gpio(resources.pi.actuators.valve.gpio, 'out');
+  for (i = 0; i < 7; i++) {
+    if (resources.pi.timer.weekofdays[i] != null) {
+      days.push(resources.pi.timer.weekofdays[i]);
+    }
+  }
+  rule.dayOfWeek = days;
+  rule.hour = resources.pi.timer.hour;
+  rule.minute = resources.pi.timer.minute;
+  rule.second = 1;
+  if (!resources.pi.actuators.valve.status) {
+    var j = schedule.scheduleJob(rule, function () {
+      console.log('Valve is start running on: ' + resources.pi.timer.weekofdays + "  " + resources.pi.timer.hour + "  " + resources.pi.timer.minute);
+      actuator.writeSync(1);
+        console.info('!!!!!!!!!!!!!!!!!Open value ');
+      resources.pi.actuators.valve.status = true;
+    });
+  }
+  console.log("weekdays " + resources.pi.timer.weekofdays);
+  console.log("hour" + resources.pi.timer.hour);
+  console.log("minut " + resources.pi.timer.minute);
+  res.redirect('/index')
+});
 
 
 module.exports = app;
